1b0d3e4ef47e2d427da0ecb5aca23590f701cfb3
Jeffrey Somarriba <31905398+J-Soma@users.noreply.github.com>
2022-09-21 11:20:10 -0600

    Actualizado moneda1.py

--- semana_4_orm_api/moneda1.py
+++ semana_4_orm_api/moneda1.py
@@ -1,11 +1,13 @@
 import requests
+import os
 
 def main():
-    respuesta = requests.get("https://api.fixer.io/latest?base=USD&symbols=EUR")
+    respuesta = requests.get("https://api.apilayer.com/fixer/convert?to=EUR&from=USD&amount=1",
+                headers={"apikey":os.get_env("API_KEY")})
     if respuesta.status_code != 200:
         raise Exception("ERROR: Consulta de API no exitosa.")
     datos = respuesta.json()
-    tasa = datos["rates"]["EUR"]
+    tasa = datos["info"]["rate"]
     print(f"1 USD es igual a {tasa} EUR")
 
 if __name__ == "__main__":
