ebc1e1e201683bfc5da1764edfd7fbc4fd3bceb8
Jeffrey Somarriba <31905398+J-Soma@users.noreply.github.com>
2022-09-21 11:20:35 -0600

    Corregido error en moneda0.py y moneda1.py

--- semana_4_orm_api/moneda1.py
+++ semana_4_orm_api/moneda1.py
@@ -3,7 +3,7 @@ import os
 
 def main():
     respuesta = requests.get("https://api.apilayer.com/fixer/convert?to=EUR&from=USD&amount=1",
-                headers={"apikey":os.get_env("API_KEY")})
+                headers={"apikey":os.getenv("API_KEY")})
     if respuesta.status_code != 200:
         raise Exception("ERROR: Consulta de API no exitosa.")
     datos = respuesta.json()
