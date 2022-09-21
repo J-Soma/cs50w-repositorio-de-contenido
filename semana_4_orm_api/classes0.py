2fb16108fc324be538195160a7966685f322c9d6
Jeffrey Somarriba <31905398+J-Soma@users.noreply.github.com>
2022-09-21 10:09:38 -0600

    Eliminada carpeta duplicada

--- semana_4_orm_api/classes0.py
+++
@@ -1,6 +0,0 @@
-class Flight:
-
-    def __init__(self, origin, destination, duration):
-        self.origin = origin
-        self.destination = destination
-        self.duration = duration
--- semana_4_orm_api/google.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def main():
-    res = requests.get("https://www.google.com/")
-    print(res.text)
-
-if __name__ == "__main__":
-    main()
